852b02c41ebb1efe0d6f9662f387d9ea0418c13e
abolfazlbeh <abolfazl.beh@gmail.com>
2022-12-14 16:12:56 +0330

    Test and Setup Config Engine (abolfazl.beh)

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"zhycan/internal/config"
+)
 
 func main() {
 	fmt.Println("Zhycan Project")
+
+	path := "/Users/abolfazl.beh/Projects/zhycan/"
+	initialMode := "dev"
+	prefix := "ZHYCAN"
+
+	err := config.CreateManager(path, initialMode, prefix)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		flag := config.GetManager().IsInitialized()
+		if flag {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println("All modules is initialized")
 }
