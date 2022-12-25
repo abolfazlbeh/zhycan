e4028e7d300ae03da3a24569703a475626b6d8ad
abolfazlbeh <abolfazl.beh@gmail.com>
2022-12-25 19:58:19 +0330

    Upgrade general utility functions to support generic (abolfazl.beh)

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -1,8 +1,8 @@
 package utils
 
 // ArrayContains - tells whether arr contains x.
-func ArrayContains(arr []string, x string) bool {
-	for _, n := range arr {
+func ArrayContains[T comparable](arr *[]T, x T) bool {
+	for _, n := range *arr {
 		if x == n {
 			return true
 		}
