74ddc412bb629868d96c1b00ee96f42a3ee26d60
abolfazlbeh <abolfazl.beh@gmail.com>
2023-04-09 19:09:39 +0330

    Filter Request Methods By Config (abolfazl.beh)

--- internal/http/types.go
+++ internal/http/types.go
@@ -5,24 +5,25 @@ type ServerConfig struct {
 	Name          string   `json:"name"`
 	Versions      []string `json:"versions"`
 	Config        struct {
-		ServerHeader         string `json:"server_header"`
-		StrictRouting        bool   `json:"strict_routing"`
-		CaseSensitive        bool   `json:"case_sensitive"`
-		UnescapePath         bool   `json:"unescape_path"`
-		Etag                 bool   `json:"etag"`
-		BodyLimit            int    `json:"body_limit"`
-		Concurrency          int    `json:"concurrency"`
-		ReadTimeout          int    `json:"read_timeout"`
-		WriteTimeout         int    `json:"write_timeout"`
-		IdleTimeout          int    `json:"idle_timeout"`
-		ReadBufferSize       int    `json:"read_buffer_size"`
-		WriteBufferSize      int    `json:"write_buffer_size"`
-		CompressedFileSuffix string `json:"compressed_file_suffix"`
-		GetOnly              bool   `json:"get_only"`
-		DisableKeepalive     bool   `json:"disable_keepalive"`
-		Network              string `json:"network"`
-		EnablePrintRoutes    bool   `json:"enable_print_routes"`
-		AttachErrorHandler   bool   `json:"attach_error_handler"`
+		ServerHeader         string   `json:"server_header"`
+		StrictRouting        bool     `json:"strict_routing"`
+		CaseSensitive        bool     `json:"case_sensitive"`
+		UnescapePath         bool     `json:"unescape_path"`
+		Etag                 bool     `json:"etag"`
+		BodyLimit            int      `json:"body_limit"`
+		Concurrency          int      `json:"concurrency"`
+		ReadTimeout          int      `json:"read_timeout"`
+		WriteTimeout         int      `json:"write_timeout"`
+		IdleTimeout          int      `json:"idle_timeout"`
+		ReadBufferSize       int      `json:"read_buffer_size"`
+		WriteBufferSize      int      `json:"write_buffer_size"`
+		CompressedFileSuffix string   `json:"compressed_file_suffix"`
+		GetOnly              bool     `json:"get_only"`
+		DisableKeepalive     bool     `json:"disable_keepalive"`
+		Network              string   `json:"network"`
+		EnablePrintRoutes    bool     `json:"enable_print_routes"`
+		AttachErrorHandler   bool     `json:"attach_error_handler"`
+		RequestMethods       []string `json:"request_methods"`
 	} `json:"conf"`
 	Middlewares struct {
 		Order []string `json:"order"`
--- internal/http/types_test.go
+++ internal/http/types_test.go
@@ -30,7 +30,8 @@ func TestServerConfig_UnmarshalJson(t *testing.T) {
         "disable_keepalive": false,
         "network": "tcp",
         "enable_print_routes": true,
-        "attach_error_handler": true
+        "attach_error_handler": true,
+		"request_methods": ["ALL"]
       },
       "middlewares": {
 		"order": ["logger", "favicon"],
@@ -51,24 +52,25 @@ func TestServerConfig_UnmarshalJson(t *testing.T) {
 		Name:          "s1",
 		Versions:      v,
 		Config: struct {
-			ServerHeader         string `json:"server_header"`
-			StrictRouting        bool   `json:"strict_routing"`
-			CaseSensitive        bool   `json:"case_sensitive"`
-			UnescapePath         bool   `json:"unescape_path"`
-			Etag                 bool   `json:"etag"`
-			BodyLimit            int    `json:"body_limit"`
-			Concurrency          int    `json:"concurrency"`
-			ReadTimeout          int    `json:"read_timeout"`
-			WriteTimeout         int    `json:"write_timeout"`
-			IdleTimeout          int    `json:"idle_timeout"`
-			ReadBufferSize       int    `json:"read_buffer_size"`
-			WriteBufferSize      int    `json:"write_buffer_size"`
-			CompressedFileSuffix string `json:"compressed_file_suffix"`
-			GetOnly              bool   `json:"get_only"`
-			DisableKeepalive     bool   `json:"disable_keepalive"`
-			Network              string `json:"network"`
-			EnablePrintRoutes    bool   `json:"enable_print_routes"`
-			AttachErrorHandler   bool   `json:"attach_error_handler"`
+			ServerHeader         string   `json:"server_header"`
+			StrictRouting        bool     `json:"strict_routing"`
+			CaseSensitive        bool     `json:"case_sensitive"`
+			UnescapePath         bool     `json:"unescape_path"`
+			Etag                 bool     `json:"etag"`
+			BodyLimit            int      `json:"body_limit"`
+			Concurrency          int      `json:"concurrency"`
+			ReadTimeout          int      `json:"read_timeout"`
+			WriteTimeout         int      `json:"write_timeout"`
+			IdleTimeout          int      `json:"idle_timeout"`
+			ReadBufferSize       int      `json:"read_buffer_size"`
+			WriteBufferSize      int      `json:"write_buffer_size"`
+			CompressedFileSuffix string   `json:"compressed_file_suffix"`
+			GetOnly              bool     `json:"get_only"`
+			DisableKeepalive     bool     `json:"disable_keepalive"`
+			Network              string   `json:"network"`
+			EnablePrintRoutes    bool     `json:"enable_print_routes"`
+			AttachErrorHandler   bool     `json:"attach_error_handler"`
+			RequestMethods       []string `json:"request_methods"`
 		}{
 			ServerHeader:         "",
 			StrictRouting:        false,
@@ -88,6 +90,7 @@ func TestServerConfig_UnmarshalJson(t *testing.T) {
 			Network:              "tcp",
 			EnablePrintRoutes:    true,
 			AttachErrorHandler:   true,
+			RequestMethods:       []string{"ALL"},
 		},
 		Middlewares: struct {
 			Order []string `json:"order"`
