20dc2bfb2831a1d530ba25060fd6ad45dca02ba9
abolfazlbeh <abolfazl.beh@gmail.com>
2023-08-19 16:29:25 +0330

    gRPC Pkg Shortcut and Register gRPC controller  (abolfazl.beh)

--- internal/engine/interfaces.go
+++ internal/engine/interfaces.go
@@ -6,3 +6,8 @@ type RestfulApp interface {
 	Routes() []http.HttpRoute
 	GetName() string
 }
+
+type GrpcApp interface {
+	GetName() string
+	GetServerNames() []string
+}
--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -2,12 +2,14 @@ package engine
 
 import (
 	"github.com/abolfazlbeh/zhycan/internal/engine"
+	interalgRPC "github.com/abolfazlbeh/zhycan/internal/grpc"
 	"github.com/abolfazlbeh/zhycan/pkg/http"
+	"google.golang.org/grpc"
 	"strings"
 )
 
 // RegisterRestfulController - register the restful controller to the engine
-func RegisterRestfulController(app engine.RestfulApp) error {
+func RegisterRestfulController(app engine.RestfulApp) {
 	routes := app.Routes()
 
 	controllerName := app.GetName()
@@ -23,7 +25,6 @@ func RegisterRestfulController(app engine.RestfulApp) error {
 	}
 
 	http.AddBulkHttpRoutes(routes)
-	return nil
 }
 
 // Controller Struct - The controller struct to be used by others
@@ -33,4 +34,30 @@ type Controller struct {
 
 func (c Controller) GetName() string {
 	return c.name
+}
+
+func RegisterGrpcController(app engine.GrpcApp, registerFunc func(server *grpc.Server, cls interface{})) {
+	controllerName := app.GetName()
+	serverNames := app.GetServerNames()
+
+	if strings.TrimSpace(controllerName) != "" {
+		return
+	}
+
+	if len(serverNames) <= 0 {
+		return
+	}
+
+	for _, item := range serverNames {
+		srv, err := interalgRPC.GetManager().GetServerByName(item)
+		if err != nil {
+			// TODO: log the error
+			continue
+		}
+		err = srv.RegisterController(registerFunc, app)
+		if err != nil {
+			// TODO: log the error
+			continue
+		}
+	}
 }
