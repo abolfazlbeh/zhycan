879fc0fe707c8f07ed968055a3ec80165e084ce7
Abolfazl Behroozi <abolfazl.beh@gmail.com>
2024-11-26 16:59:43 +0330

    Creating a command to compile protobuf file and generate the gRPC files

--- internal/command/compile.go
+++ internal/command/compile.go
@@ -2,13 +2,18 @@ package command
 
 import (
 	"fmt"
+	"github.com/abolfazlbeh/zhycan/internal/config"
 	"github.com/spf13/cobra"
 	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 const (
-	RunCompileCommandInitMsg  = `Zhycan > Compiling the protobuf file ...`
-	RunCompileCommandFileName = `Zhycan > "%s.proto" file is going to be compiled ...`
+	RunCompileCommandInitMsg      = `Zhycan > Compiling the protobuf file ...`
+	RunCompileCommandFileName     = `Zhycan > "%s.proto" file is going to be compiled ...`
+	RunCompileCommandError        = `Zhycan > Compiling encountered the error: %s`
+	RunCompileCommandFileNotExist = `Zhycan > %s.proto file does not exist in "%s"`
 )
 
 func NewCompileCommandCmd() *cobra.Command {
@@ -33,9 +38,54 @@ func runCompileCmdExecute(cmd *cobra.Command, args []string) {
 
 	ex, err := os.Getwd()
 	if err != nil {
-		fmt.Fprintf(cmd.OutOrStdout(), err.Error())
-	} else {
-		fmt.Fprintf(cmd.OutOrStdout(), "Current Path: %s", ex)
+		fmt.Fprintf(cmd.OutOrStdout(), RunCompileCommandError, err.Error())
+		return
 	}
 
+	// Let's create a path for file and the compiled folder
+	// Get the appropriate "proto" folder path from the config
+
+	protoFolderPath, err := config.GetManager().Get("protobuf", "src_dir")
+	if err != nil {
+		protoFolderPath = "app/proto"
+	}
+
+	desiredProtoPath := filepath.Join(ex, protoFolderPath.(string))
+
+	// Let's find the file
+	protobufFilePath := filepath.Join(desiredProtoPath, args[0]+".proto")
+	if _, err1 := os.Stat(protobufFilePath); os.IsNotExist(err1) {
+		// file does not exist --> Show error and exit
+		fmt.Fprintf(cmd.OutOrStdout(), RunCompileCommandFileNotExist, args[0]+".proto", desiredProtoPath)
+		return
+	}
+
+	// File exist --> go to create the folder
+	compiledFolderPath := filepath.Join(desiredProtoPath, args[0])
+	err2 := os.Mkdir(compiledFolderPath, os.ModePerm)
+	if err2 != nil {
+		fmt.Fprintf(cmd.OutOrStdout(), RunCompileCommandError, err.Error())
+		return
+	}
+
+	// protoc --go_out=./exchange --go_opt=paths=source_relative --go-grpc_out=./exchange --go-grpc_opt=paths=source_relative exchange.proto
+	commandToBeExecuted := fmt.Sprintf("--go_out=%s --go_opt=paths=source_relative --go-grpc_out=%s --go-grpc_opt=paths=source_relative %s",
+		compiledFolderPath, compiledFolderPath, protobufFilePath)
+
+	protoCmd := exec.Command("protoc")
+	protoCmd.Args = append(protoCmd.Args, commandToBeExecuted)
+	//gitCmd.Args = append(gitCmd.Args, "--initial-branch=main")
+	protocErr := protoCmd.Start()
+	if protocErr != nil {
+		fmt.Fprintln(cmd.OutOrStdout())
+		fmt.Fprintf(cmd.OutOrStdout(), RunCompileCommandError, protocErr)
+		return
+	}
+
+	protocErr = protoCmd.Wait()
+	if protocErr != nil {
+		fmt.Fprintln(cmd.OutOrStdout())
+		fmt.Fprintf(cmd.OutOrStdout(), RunCompileCommandError, protocErr)
+		return
+	}
 }
